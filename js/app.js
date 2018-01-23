0455e102cfb07a84633abfdfc41eadf78f00283a
carmenvegga <maricarmenvegaislas@gmail.com>
2018-01-23 09:14:14 -0600

    Primeros avances en lyft

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".bg-out").fadeOut(1000);
+    },1000);
+
+    setTimeout(function() {
+        $(".bg-in").fadeIn(1000);
+    },2000);
+});
+
+
+/*  function(){
+    $(".btn1").click(function(){
+        $("p").fadeOut(1000)
+    });
+    $(".btn2").click(function(){
+        $("p").fadeIn(1000);
+    });
+}); */
